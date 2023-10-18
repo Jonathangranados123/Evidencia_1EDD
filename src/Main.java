cc8d8a91274f16948da63f8f31028c32169830e5
Arnulfo-Ramos168 <arrsaurio@gmail.com>
2023-10-18 14:49:32 -0600

    hola

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import Evidencia1_EstructuraDeDatos.CLI.QueueSimCli;
 
 class Main {
     public static void main( String[] args ) {
+        //hola com´pañeros
         QueueSimCli.initCli();
         QueueSimCli.startSim();
     }
