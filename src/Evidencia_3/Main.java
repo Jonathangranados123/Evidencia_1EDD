6c57ea4143fa18578e6f5f86a88fec6dd943bc60
Jonathangranados123 <123481552+Jonathangranados123@users.noreply.github.com>
2023-12-01 23:29:51 -0600

    EVIDENCIA TREEEES!!!!!!!!!!!!!!!!!!!!!!!

---
+++ src/Evidencia_3/Main.java
@@ -0,0 +1,32 @@
+package Evidencia_3;
+
+import Evidencia_3.CLI.AnimalGame;
+import Evidencia_3.utils.BTNode;
+
+/**
+ * Clase principal que contiene el método main para ejecutar el juego de adivinanzas de animales.
+ */
+ public class Main {
+
+    /**
+     * Método principal que inicia el juego de adivinanzas de animales.
+     *
+     * @param args Argumentos de línea de comandos (no se utilizan).
+     */
+    public static void main(String[] args) {
+        BTNode<String> root;
+
+        // Muestra las instrucciones del juego.
+        AnimalGame.instruct();
+
+        // Inicializa el árbol de adivinanzas.
+        root = AnimalGame.beginningTree();
+
+        // Realiza el juego hasta que el usuario decida no jugar más.
+        do {
+            AnimalGame.play(root);
+        } while (AnimalGame.query("¿Volvemos a jugar?"));
+
+        System.out.println("Gracias por enseñarme una que otra cosa.");
+    }
+}
